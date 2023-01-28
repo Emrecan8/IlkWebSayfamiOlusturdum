e05bff4d402146cb2be10f94bbcf1a20c00d9c7d
Emrecan <emrecanyasar8@outlook.com>
2023-01-28 13:41:19 +0300

    Boolean Veri Türü İle Çalışmak

--- js_dersleri/05-boolen-veri-turu.js
+++ js_dersleri/05-boolen-veri-turu.js
@@ -1,3 +1,23 @@
+// 0 ile 1 'i anlamak
 let isActive = false
 isActive =true
+console.log(isActive)
 
+let username
+let isUserName =Boolean(username)
+console.log(isUserName)
+
+Boolean("11")
+Boolean("0")
+Boolean("")
+
+username ="user"
+
+console.log("User Name:",Boolean(username))
+
+//0,-0,null,false,Nan ,undefined,("")
+
+Boolean(0)
+Boolean(-0)
+Boolean(-0.1)
+Boolean(0===0)
