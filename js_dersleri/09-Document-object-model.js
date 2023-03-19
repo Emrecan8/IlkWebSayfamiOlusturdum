8cb10ed3a57d0bb749507abbf79339c4ba5e1bd5
Emrecan <emrecanyasar8@outlook.com>
2023-03-19 19:59:02 +0300

    DOM

---
+++ js_dersleri/09-Document-object-model.js
@@ -0,0 +1,10 @@
+//pathname
+console.log(document.location.hostname)
+
+console.log(document.body)
+console.log(document.head)
+
+console.log(document.URL)
+console.log(document.baseURI)
+
+document.body.style.backgroundColor="aqua"
