07b0ad51db4b6e45f0942943d700f87f6535e5ed
Emrecan <emrecanyasar8@outlook.com>
2023-03-23 02:51:22 +0300

    Karşılaştırma Operatörleri ve Mantıksal Operatörler

---
+++ js_dersleri/014-karsilastirma-operatorleri-ve-mantiksal-operatorler.js
@@ -0,0 +1,24 @@
+let price = 1
+let user ="guest"
+
+console.log(price==100)
+console.log(price==1)
+
+console.log(price===1)
+console.log(price===100)
+
+console.log(price!=10)
+
+console.log(price>10)
+console.log(price<10)
+
+console.log(price>=10)
+console.log(price<=10)
+
+
+console.log(price < 0 && user!="guest")
+console.log(price <= 0 || user!="guest")
+
+console.log(price >= 0 && !user =="guest")
+
+
