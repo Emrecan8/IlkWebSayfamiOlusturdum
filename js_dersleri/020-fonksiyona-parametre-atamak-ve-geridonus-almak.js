7abac7cc367fc73657b9d30b29aa2ea21918ff00
Emrecan <emrecanyasar8@outlook.com>
2023-03-24 01:46:52 +0300

    Fonksiyona Parametre(params) ve Geri dönüş(return) Eklemek

--- js_dersleri/020-fonksiyona-parametre-atamak-ve-geridonus-almak.js
+++ js_dersleri/020-fonksiyona-parametre-atamak-ve-geridonus-almak.js
@@ -1,16 +1,48 @@
 
-function helloWorld (){
-    console.log("Hello World")
+// function helloWorld (){
+//     console.log("Hello World")
+// }
+
+// function hello(){
+//     console.log("merhaba")
+//     helloWorld()
+// }
+
+// function userCheck (){
+
+// }
+
+// hello()
+
+// let firstName ="Lorem"
+
+// function greetings(firstName){
+//     console.log(`Merhaba ${firstName}`);
+// }
+
+// greetings()
+
+
+function greetings(firstName=""){
+    console.log(`Merhaba ${firstName ? firstName : ""}`);
 }
 
-function hello(){
-    console.log("merhaba")
-    helloWorld()
+greetings("emre")
+
+function greetings2(firstName,lastName){
+    let info =`Merhaba ${firstName} ${lastName}`
+    return info
 }
 
-function userCheck (){
-    
+let greetingInfo = greetings2("Ad" , "Soyad")   
+console.log(greetingInfo);    
+
+function domIdWriteInfo(id,info){
+    let domObject =document.querySelector(`#${id}`)
+    domObject.innerHTML=info
 }
 
-hello()
+let htmlInfo =`<span style ="color:red">asdasdgajhsd</span>`
 
+domIdWriteInfo('greeting',greetings2("Ad" , "Soyad"))
+domIdWriteInfo('greeting',htmlInfo)
