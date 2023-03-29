9c3457134b4f776327e743336cbea83feada2ab2
Emrecan <emrecanyasar8@outlook.com>
2023-03-29 03:47:28 +0300

    Fetch API ile Çalışmak

---
+++ js_dersleri/044-fetch-api-ile-calismak.js
@@ -0,0 +1,18 @@
+fetch("/data/setting.json").then(
+    response => response.json()
+  ).then(responseJson => {
+    console.log(responseJson)
+    console.log(responseJson.userName)
+  })
+  
+  let userListDOM = document.querySelector("#userList")
+
+  fetch("https://jsonplaceholder.typicode.com/posts").then(
+  response => response.json()
+).then(responseJson => {
+  responseJson.forEach(item => {
+    let liDOM = document.createElement('li')
+    liDOM.innerHTML = item.title
+    userListDOM.appendChild(liDOM)
+  })
+})
